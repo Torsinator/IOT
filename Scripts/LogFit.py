1cb194e284e1c6ab0a263f8705c0cb371b005a40
Tors Webster <websteto@outlook.com>
2025-03-04 16:35:18 +1100

    Update LogFit Script

--- Scripts/LogFit.py
+++ Scripts/LogFit.py
@@ -1,10 +1,19 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('')
-x = df[0].to_numpy()
-y = df[1].to_numpy()
+df = pd.read_csv('Data\\light_sensor.csv')
+x = df["lux-meter"].to_numpy(dtype=np.float32)
+y = df["light sensor"].to_numpy(dtype=np.float32)
+
+print(x)
+print(y)
 
 params = np.polyfit(np.log(x), y, 1)
 
-print("y = {params[0]}ln(x) + {params[1]}")
+print(f"y = {params[0]}ln(x) + {params[1]}")
+
+plt.plot(x, y)
+plt.xlabel('ADC value')
+plt.ylabel('Lux')
+plt.show()
